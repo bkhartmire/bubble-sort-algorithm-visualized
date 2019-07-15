4aa2852206c4f86bb66a7b549bf6b9f18d90f5a6
Wilson and Matthew Weidner <bkhartmire@gmail.com>
2019-07-15 18:59:10 +0900

    finish building out basic visualization functionality

--- src/app.js
+++ src/app.js
@@ -11,23 +11,45 @@ require("./index.css");
 
 const bubbleSorter = new BubbleSort([12, 2, 6, 4, 8]);
 const arr = bubbleSorter.array;
-let visualizationComplete = false;
+// let visualizationComplete = false;
 // bubblesorter.sort();
-
-// showComparison(12, 2, "box1");
-const interval = setInterval(beginCycle, 2000);
+const button = document.getElementById("begin");
+let cycleInterval;
+button.addEventListener("click", () => {
+  showIteration();
+  beginCycle();
+  cycleInterval = setInterval(beginCycle, 3000);
+});
 
 let i = 0;
+let needToSwap = false;
+let completedIterations = 0;
 function beginCycle() {
+  if (i > 0) {
+    const previousBoxId = `box${i}`;
+    clearComparisonBox(previousBoxId);
+  }
+  if (needToSwap) {
+    showSwap(i - 1, i);
+    needToSwap = false;
+  }
   if (i < arr.length - 1) {
     const boxId = "box" + (i + 1);
     showComparison(i, i + 1, boxId);
     i++;
-  } else {
-    clearInterval(interval);
-    visualizationComplete = true;
+  } else if (completedIterations < arr.length) {
     i = 0;
+    completedIterations++;
+    showIteration();
+  } else {
+    clearInterval(cycleInterval);
   }
+}
+
+function showIteration() {
+  const element = document.getElementById("iteration");
+  const string = "Iteration " + (completedIterations + 1);
+  element.innerText = string;
 }
 
 function showComparison(index1, index2, boxId) {
@@ -37,8 +59,21 @@ function showComparison(index1, index2, boxId) {
   const string = `${item1} < ${item2}`;
   box.innerHTML = "<h1>" + string + "</h1>";
   if (item1 > item2) {
+    needToSwap = true;
   }
+}
+
+function showSwap(index1, index2) {
+  const firstId = `item${index1}`;
+  const secondId = `item${index2}`;
+  document.getElementById(firstId).innerText = arr[index2];
+  document.getElementById(secondId).innerText = arr[index1];
   bubbleSorter.swap(index1, index2);
+}
+
+function clearComparisonBox(boxId) {
+  const box = document.getElementById(boxId);
+  box.innerText = "";
 }
 
 // function createCheesyTitle(slogan) {
