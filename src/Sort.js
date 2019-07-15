bf8bb5174dd1a3f43a1b05828a5c1d1a71701d35
Wilson and Matthew Weidner <bkhartmire@gmail.com>
2019-07-15 16:18:32 +0900

    finish algorithm

--- src/Sort.js
+++ src/Sort.js
@@ -3,7 +3,19 @@ class BubbleSort {
     this.array = array;
   }
   sort() {
-    console.log("sort!");
+    const arr = this.array;
+    let counter = 0;
+    while (counter < arr.length) {
+      for (let i = 0; i < arr.length - 1; i++) {
+        const originalFirst = arr[i];
+        const originalSecond = arr[i + 1];
+        if (originalFirst > originalSecond) {
+          arr[i] = originalSecond;
+          arr[i + 1] = originalFirst;
+        }
+      }
+      counter++;
+    }
   }
 }
 
